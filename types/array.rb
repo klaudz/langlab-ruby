85373321c780529bca1955eef4351f9b17925fac
klaudz <klaudzliang@gmail.com>
2020-05-15 21:55:41 +0800

    refactor(print): Optimize the way of printing results

--- types/array.rb
+++ types/array.rb
@@ -2,25 +2,19 @@
 
 def create_array
 
-    array_a0 = [ false, 1, "2", 3.0 ]       # => [false, 1, "2", 3.0]
-    array_a1 = [ "a", "b", "c", "d" ]       # => ["a", "b", "c", "d"]
+    r = []
 
-    puts "#{array_a0}"
-    puts "#{array_a1}"
+    r << [ false, 1, "2", 3.0 ]         # => [false, 1, "2", 3.0]
+    r << [ "a", "b", "c", "d" ]         # => ["a", "b", "c", "d"]
 
-    array_b0 = Array.new                    # => []
-    array_b1 = Array.new(4)                 # => [nil, nil, nil, nil]
-    array_b2 = Array.new(4, "str")          # => ["str", "str", "str", "str"]
-    
-    puts "#{array_b0}"
-    puts "#{array_b1}"
-    puts "#{array_b2}"
+    r << Array.new                      # => []
+    r << Array.new(4)                   # => [nil, nil, nil, nil]
+    r << Array.new(4, "str")            # => ["str", "str", "str", "str"]
 
-    array_c0 = Array.new(4) { "str" }       # => ["str", "str", "str", "str"]
-    array_c1 = Array.new(4) { |i| i.to_s }  # => ["0", "1", "2", "3"]
+    r << Array.new(4) { "str" }         # => ["str", "str", "str", "str"]
+    r << Array.new(4) { |i| i.to_s }    # => ["0", "1", "2", "3"]
 
-    puts "#{array_c0}"
-    puts "#{array_c1}"
+    r.each { |e| puts "#{e}" }
 
 end
 
