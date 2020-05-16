42e1bfedf5bf3f0a33ebe00a1e308263733bf895
klaudz <klaudzliang@gmail.com>
2020-05-16 18:29:08 +0800

    feature(array): Print results by '_utils/printer'

--- types/array.rb
+++ types/array.rb
@@ -1,73 +1,67 @@
 #!/usr/bin/ruby
 
+require_relative '_utils/printer'
+
+$p = Langlab::Printer.new
+
 def create_arrays
 
     puts "----- Create Array ---------------"
-    r = []
 
-    r << [ false, 1, "2", 3.0 ]         # => [false, 1, "2", 3.0]
-    r << [ "a", "b", "c", "d" ]         # => ["a", "b", "c", "d"]
+    $p << [ false, 1, "2", 3.0 ]         # => [false, 1, "2", 3.0]
+    $p << [ "a", "b", "c", "d" ]         # => ["a", "b", "c", "d"]
 
-    r << Array.new                      # => []
-    r << Array.new(4)                   # => [nil, nil, nil, nil]
-    r << Array.new(4, "str")            # => ["str", "str", "str", "str"]
+    $p << Array.new                      # => []
+    $p << Array.new(4)                   # => [nil, nil, nil, nil]
+    $p << Array.new(4, "str")            # => ["str", "str", "str", "str"]
 
-    r << Array.new(4) { "str" }         # => ["str", "str", "str", "str"]
-    r << Array.new(4) { |i| i.to_s }    # => ["0", "1", "2", "3"]
-
-    r.each { |e| puts "#{e}" }
+    $p << Array.new(4) { "str" }         # => ["str", "str", "str", "str"]
+    $p << Array.new(4) { |i| i.to_s }    # => ["0", "1", "2", "3"]
 
 end
 
 def access_elements
 
     puts "----- Access Elements ---------------"
-    r = []
 
     array = [ 0, 1, 2, 3, 4, 5, 6 ]
     
-    r << array[1]       # => 1
-    r << array[9]       # => nil
-    r << array[-1]      # => 6
-    r << array[-9]      # => nil
+    $p << array[1]       # => 1
+    $p << array[9]       # => nil
+    $p << array[-1]      # => 6
+    $p << array[-9]      # => nil
 
     # [start, length]
-    r << array[2, 4]    # => [2, 3, 4, 5]
+    $p << array[2, 4]    # => [2, 3, 4, 5]
 
     # [start..end]
-    r << array[2..4]    # => [2, 3, 4]
-    r << array[2..-1]   # => [2, 3, 4, 5, 6]
-    r << array[2..-6]   # => []
-    r << array[-4..-2]  # => [3, 4, 5]
-
-    r.each { |e| puts e ? "#{e}" : "nil" }
+    $p << array[2..4]    # => [2, 3, 4]
+    $p << array[2..-1]   # => [2, 3, 4, 5, 6]
+    $p << array[2..-6]   # => []
+    $p << array[-4..-2]  # => [3, 4, 5]
 
 end
 
 def obtain_information
 
     puts "----- Obtain Information ---------------"
-    r = []
 
     array = [ 0, 1, 2, 3, 4, 5, 6 ]
 
-    r << array.length       # => 7
-    r << array.count        # => 7
-    r << array.size         # => 7
+    $p << array.length       # => 7
+    $p << array.count        # => 7
+    $p << array.size         # => 7
 
-    r << array.empty?       # => false
+    $p << array.empty?       # => false
 
-    r << array.include?(3)  # => true
-    r << array.include?(7)  # => false
-
-    r.each { |e| puts "#{e}" }
+    $p << array.include?(3)  # => true
+    $p << array.include?(7)  # => false
     
 end
 
 def iterate_arrays
 
     puts "----- Iterate Arrays ---------------"
-    r = []
 
     # `each` and `reverse_each`
     array_0 = [ 0, 1, 2, 3 ]
@@ -75,25 +69,23 @@ def iterate_arrays
     array_2 = []
     array_0.each         { |i| array_1 << i ** 2 }
     array_0.reverse_each { |i| array_2 << i ** 2 }
-    r << array_0    # => [0, 1, 2, 3]
-    r << array_1    # => [0, 1, 4, 9]
-    r << array_2    # => [9, 4, 1, 0]
+    $p << array_0    # => [0, 1, 2, 3]
+    $p << array_1    # => [0, 1, 4, 9]
+    $p << array_2    # => [9, 4, 1, 0]
 
     # <<< `map` and `map!`
 
     old_array = [ "a", "b", "c", "d" ]
     new_array = old_array.map { |item| "_" + item }
-    r << old_array  # => ["a", "b", "c", "d"]
-    r << new_array  # => ["_a", "_b", "_c", "_d"]
+    $p << old_array  # => ["a", "b", "c", "d"]
+    $p << new_array  # => ["_a", "_b", "_c", "_d"]
 
     old_array = [ "a", "b", "c", "d" ]
     new_array = old_array.map! { |item| "_" + item }
-    r << old_array  # => ["_a", "_b", "_c", "_d"]
-    r << new_array  # => ["_a", "_b", "_c", "_d"]
+    $p << old_array  # => ["_a", "_b", "_c", "_d"]
+    $p << new_array  # => ["_a", "_b", "_c", "_d"]
 
     # >>>
-
-    r.each { |e| puts "#{e}" }
 
 end
 
