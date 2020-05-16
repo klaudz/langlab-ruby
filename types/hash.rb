1c37efb64471b787f5979d36bd21fb63558fd98e
klaudz <klaudzliang@gmail.com>
2020-05-16 22:20:32 +0800

    feature(hash): Add samples of getting and setting keys and values

--- types/hash.rb
+++ types/hash.rb
@@ -40,6 +40,31 @@ def create_hashes
 
 end
 
+def get_set_keys_values
+
+    puts "----- Get Keys and Values ---------------", ""
+
+    hash = { "k0" => "v0", "k1" => "v1", :k2 => "v2" }
+    
+    $p << hash["k0"]    #=> "v0"
+    $p << hash["k1"]    #=> "v1"
+    $p << hash["k2"]    #=> nil
+    $p << hash[:k2]     #=> "v2"
+    $p.new_line
+
+    puts "----- Set Keys and Values ---------------", ""
+
+    hash = { "k0" => "v0", :k0 => "v0" }
+    hash["k0"] = "_v0"  # Update value
+    hash[:k0] = "_v0"   # Update value
+    hash["k1"] = "_v1"  # Add key and value
+    hash[2] = 2
+    hash[nil] = nil
+    $p << hash          #=> {"k0"=>"_v0", :k0=>"_v0", "k1"=>"_v1", 2=>2, nil=>nil}
+    $p.new_line
+
+end
+
 def obtain_information
 
     puts "----- Obtain Information ---------------", ""
@@ -76,5 +101,6 @@ def iterate_hashes
 end
 
 create_hashes
+get_set_keys_values
 obtain_information
 iterate_hashes
