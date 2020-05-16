34c9c70d3c8a063bc9100ae7f5974e800bf20504
klaudz <klaudzliang@gmail.com>
2020-05-17 01:32:35 +0800

    feature(json): Add samples of generating and parsing JSON

---
+++ data/json.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/ruby
+
+require 'json'
+require_relative '_utils/printer'
+
+$p = Langlab::Printer.new
+$p.new_line
+
+def generate_json
+
+    puts "----- Generate JSON ---------------", ""
+
+    object = {
+        "bool" => true,
+        "number" => 18,
+        "string" => "string",
+        "array" => [ 0, 1 ],
+        "hash" => { "k0" => "v0", "k1" => "v1" },
+    }
+    $p << object.to_json            #=> '{"bool":true,"number":18,"string":"string","array":[0,1],"hash":{"k0":"v0","k1":"v1"}}'
+    $p << JSON.generate(object)     #=> '{"bool":true,"number":18,"string":"string","array":[0,1],"hash":{"k0":"v0","k1":"v1"}}'
+    $p << JSON.generate(object, {   #=> '{
+        :indent => "  ",            #     "bool": true,
+        :space => " ",              #     "number": 18,
+        :object_nl => "\n",         #     "string": "string",
+        :array_nl => "\n"           #     "array": [
+    })                              #       0,
+                                    #       1
+                                    #     ],
+                                    #     "hash": {
+                                    #       "k0": "v0",
+                                    #       "k1": "v1"
+                                    #     }
+                                    #   }'
+    $p.new_line
+
+end
+
+def parse_json
+
+    puts "----- Parse JSON ---------------", ""
+
+    json_string = '{"bool":true,"number":18,"string":"string","array":[0,1],"hash":{"k0":"v0","k1":"v1"}}'
+    $p << JSON.parse(json_string)   #=> {"bool"=>true, "number"=>18, "string"=>"string", "array"=>[0, 1], "hash"=>{"k0"=>"v0", "k1"=>"v1"}}
+    $p.new_line
+
+end
+
+generate_json
+parse_json
