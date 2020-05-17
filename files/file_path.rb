5f48e8ec74b908e3b3ba77d43080df1535b7f6b1
klaudz <klaudzliang@gmail.com>
2020-05-17 12:08:38 +0800

    feature(file_path): Add samples of working with absolute paths

---
+++ files/file_path.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require_relative '../_utils/printer'
+
+$p = Langlab::Printer.new
+$p.new_line
+
+def work_with_absolute_paths
+
+    puts "----- Work with Absolute Paths ---------------", ""
+
+    # <<< `absolute_path`
+
+    # Remark: Assumes the working directory is "/Users/klaudz"
+    $p << File.absolute_path("")                        #=> "/Users/klaudz"
+    $p << File.absolute_path(".")                       #=> "/Users/klaudz"
+    $p << File.absolute_path("file_name")               #=> "/Users/klaudz/file_name"
+    $p << File.absolute_path("/Users/klaudz/file_name") #=> "/Users/klaudz/file_name"
+    $p.new_line
+
+    # Remark: Assumes the working directory is "/Users/klaudz"
+    $p << File.absolute_path("file_name", "/Users/cat") #=> "/Users/cat/file_name"
+    $p << File.absolute_path("file_name", "")           #=> "/Users/klaudz/file_name"
+    $p << File.absolute_path("file_name", ".")          #=> "/Users/klaudz/file_name"
+    $p << File.absolute_path("file_name", "dir_name")   #=> "/Users/klaudz/dir_name/file_name"
+    $p << File.absolute_path("/Users/klaudz/file_name", "/Users/cat")
+                                                        #=> "/Users/klaudz/file_name"
+    $p.new_line
+
+    # >>>
+
+end
+
+work_with_absolute_paths
